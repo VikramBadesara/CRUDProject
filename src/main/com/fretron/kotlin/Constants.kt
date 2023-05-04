af5d1c54b8640dd92e9b39e749ea574c227308b8
VikramBadesara <Vikram.Badesara@fretron.com>
2023-05-04 18:05:13 +0530

    n

---
+++ src/main/com/fretron/kotlin/Constants.kt
@@ -0,0 +1,8 @@
+import
+
+
+// ==========================CRUDPROJECT SERVICE URL=======================================================
+const val CRUDPROJECT_APP_IP = "kotlin.restservice.ip"
+const val CRUDPROJECT_APP_PORT = "kotlin.restservice.port"
+
+
---
+++ src/main/com/fretron/kotlin/di/ConfigModule.kt
@@ -0,0 +1,15 @@
+package di
+
+import dagger.Module
+import dagger.Provides
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+class ConfigModule {
+
+//    @Provides
+//    @Named(CRUDPROJECT_APP_IP)
+
+
+}
